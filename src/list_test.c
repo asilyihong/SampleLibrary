9417a6712d9965d276172ae88bbc920bcd278881
asil <asil.yihong@gmail.com>
2013-08-17 21:17:51 +0800

    change name of main and add content

---
+++ src/list_test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "y_list.h"
+
+int main(int argc, char **argv)
+{
+    int sample[] = {2, 5, 3, 6, 9};
+    int res = 0, index;
+    YList *list = YList_new();
+    for(index = 0; index < 5; index++) {
+        res = YList_add(list, &sample[index]);
+        printf("res == %d\n", res);
+    }
+
+    res = YList_removeItem(list, &sample[0]);
+    printf("res == %d\n", res);
+    res = YList_removeIndex(list, 0);
+    printf("res == %d\n", res);
+    // this should out of bound
+    res = YList_removeIndex(list, 4);
+    printf("res == %d\n", res);
+
+
+    return EXIT_SUCCESS;
+}
